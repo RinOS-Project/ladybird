82687a054c25993451717e5907ab85b25ae290a8
ysmreg <33682244+ysmreg@users.noreply.github.com>
2026-09-09 12:50:44 +0900

    feat: implement Web Serial validation boundary

--- Libraries/LibWeb/Serial/Serial.cpp
+++ Libraries/LibWeb/Serial/Serial.cpp
@@ -6,6 +6,7 @@
 
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/Bindings/SerialPrototype.h>
+#include <LibJS/Runtime/Array.h>
 #include <LibWeb/HTML/EventNames.h>
 #include <LibWeb/HTML/Scripting/Environments.h>
 #include <LibWeb/HTML/Scripting/TemporaryExecutionContext.h>
@@ -31,71 +32,50 @@ void Serial::initialize(JS::Realm& realm)
 }
 
 // https://wicg.github.io/serial/#requestport-method
-WebIDL::ExceptionOr<GC::Ref<WebIDL::Promise>> Serial::request_port(SerialPortRequestOptions const)
+WebIDL::ExceptionOr<GC::Ref<WebIDL::Promise>> Serial::request_port(SerialPortRequestOptions const options)
 {
     auto& realm = this->realm();
-
-    // FIXME: 1. Let promise be a new promise.
-
-    // FIXME: 2. If this's relevant global object's associated Document is not allowed to use the policy-controlled feature named "serial",
-    //           reject promise with a "SecurityError" DOMException and return promise.
-
-    // FIXME: 3. If the relevant global object of this does not have transient activation, reject promise with a "SecurityError" DOMException and return promise.
-
-    // FIXME: 4. If options["filters"] is present, then for each filter in options["filters"] run the following steps:
-
-    // FIXME: 5. Run the following steps in parallel:
-    {
-        // FIXME: 1. Let allPorts be an empty list.
-
-        // FIXME: 2. For each Bluetooth device registered with the system:
-
-        // FIXME: 3. For each available non-Bluetooth serial port:
-        {
-            // FIXME: 1. Let port be a SerialPort representing the port.
-
-            // FIXME: 2. Append port to allPorts.
+    if (HTML::is_non_secure_context(HTML::relevant_settings_object(*this)))
+        return WebIDL::create_rejected_promise_from_exception(realm,
+            WebIDL::SecurityError::create(realm, "Web Serial requires a secure context"_utf16));
+    if (!is<HTML::Window>(realm.global_object()) ||
+        !as<HTML::Window>(realm.global_object()).has_transient_activation())
+        return WebIDL::create_rejected_promise_from_exception(realm,
+            WebIDL::SecurityError::create(realm, "Web Serial requires transient user activation"_utf16));
+    if (options.allowed_bluetooth_service_class_ids.has_value() &&
+        !options.allowed_bluetooth_service_class_ids->is_empty())
+        return WebIDL::create_rejected_promise_from_exception(realm,
+            WebIDL::NotSupportedError::create(realm, "Bluetooth serial is not supported"_utf16));
+    if (options.filters.has_value()) {
+        if (options.filters->is_empty() || options.filters->size() > 16u)
+            return WebIDL::create_rejected_promise_from_exception(realm,
+                JS::TypeError::create(realm, "Serial filters must not be empty or oversized"sv));
+        for (auto const& filter : *options.filters) {
+            if (!filter.usb_vendor_id.has_value() && !filter.usb_product_id.has_value() &&
+                !filter.bluetooth_service_class_id.has_value())
+                return WebIDL::create_rejected_promise_from_exception(realm,
+                    JS::TypeError::create(realm, "A serial filter must select a device"sv));
+            if (filter.bluetooth_service_class_id.has_value())
+                return WebIDL::create_rejected_promise_from_exception(realm,
+                    WebIDL::NotSupportedError::create(realm, "Bluetooth serial is not supported"_utf16));
         }
-
-        // FIXME: 4. Prompt the user to grant the site access to a serial port by presenting them with a list of ports
-        //           in allPorts that match any filter in options["filters"] if present and allPorts otherwise.
-
-        // FIXME: 5. If the user does not choose a port, queue a global task on the relevant global object of this using the
-        //           serial port task source to reject promise with a "NotFoundError" DOMException and abort these steps.
-
-        // FIXME: 6. Let port be a SerialPort representing the port chosen by the user.
-
-        // FIXME: 7. Queue a global task on the relevant global object of this using the serial port task source to resolve promise with port.
     }
-
-    // 6. Return promise.
-    dbgln("FIXME: Unimplemented Serial::request_port()");
-    return WebIDL::create_rejected_promise(realm, WebIDL::UnknownError::create(realm, Utf16String {}));
+    /* The authenticated RinOS device portal is the only source of physical
+     * ports.  Until that portal is attached to this WebContent instance,
+     * expose the specified Web Serial failure instead of inventing a port or
+     * leaking a path string. */
+    return WebIDL::create_rejected_promise_from_exception(realm,
+        WebIDL::NotFoundError::create(realm, "No serial device is available"_utf16));
 }
 
 // https://wicg.github.io/serial/#getports-method
 GC::Ref<WebIDL::Promise> Serial::get_ports()
 {
     auto& realm = this->realm();
-
-    // FIXME: 1. Let promise be a new promise.
-
-    // FIXME: 2. If this's relevant global object's associated Document is not allowed to use the policy-controlled feature named "serial",
-    //         reject promise with a "SecurityError" DOMException and return promise.
-
-    // FIXME: 3. Run the following steps in parallel:
-    {
-        // FIXME: 1. Let availablePorts be the sequence of available serial ports which the user has allowed the site to
-        //           access as the result of a previous call to requestPort().
-
-        // FIXME: 2. Let ports be the sequence of the SerialPorts representing the ports in availablePorts.
-
-        // FIXME: 3. Queue a global task on the relevant global object of this using the serial port task source to resolve promise with ports.
-    }
-
-    // 4. Return promise.
-    dbgln("FIXME: Unimplemented Serial::get_ports()");
-    return WebIDL::create_rejected_promise(realm, WebIDL::UnknownError::create(realm, Utf16String {}));
+    if (HTML::is_non_secure_context(HTML::relevant_settings_object(*this)))
+        return WebIDL::create_rejected_promise_from_exception(realm,
+            WebIDL::SecurityError::create(realm, "Web Serial requires a secure context"_utf16));
+    return WebIDL::create_resolved_promise(realm, JS::Array::create(realm, 0));
 }
 
 // https://wicg.github.io/serial/#onconnect-attribute
